60641c63399492a03a08a4122c5b3e582aea397b
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-04-27 13:59:27 +0500

    comments in js

---
+++ 03_JAVASCRIPT/01_The_Console/03_comments.js
@@ -0,0 +1,6 @@
+console.log("hello world"); // this is hello world commment
+
+/*Coca-Cola formula.
+Kentucky Fried Chicken (KFC)'s 11 herbs and spices.
+The sauce used in a Big Mac at McDonald's.
+*/
