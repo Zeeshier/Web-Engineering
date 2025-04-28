2c15e4c19e4af46c4599678488d7890e22f5e970
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-04-28 22:53:15 +0500

    operators

---
+++ 03_JAVASCRIPT/02_Variables/3_operators.js
@@ -0,0 +1,17 @@
+let score = 0;       
+
+score = 4 + 3;      
+score = 4 - 3;     
+score = 4 * 3;       
+score = 4 / 3;       
+score = 4 % 3;      
+
+console.log(score);   
+
+
+// temperature conversion from Fahrenheit to Celsius
+let fahrenheit = 75;
+let celsius;
+
+celsius = (fahrenheit - 32) / 1.8;
+console.log(celsius)
