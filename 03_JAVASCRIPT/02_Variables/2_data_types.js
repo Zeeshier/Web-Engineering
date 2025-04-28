7db3333944e52eef09c896595705fb89b1c3f713
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-04-28 22:47:12 +0500

    data types

---
+++ 03_JAVASCRIPT/02_Variables/2_data_types.js
@@ -0,0 +1,9 @@
+let companyName = "Meta";
+let foundingYear = 2004;
+let isActive = true;
+let fundingAmount;
+
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
