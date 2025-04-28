ffa4f66261caaa7c8790894673318a2b285ce972
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-04-28 22:40:50 +0500

    let and const

---
+++ 03_JAVASCRIPT/02_Variables/1_ let_and_const.js
@@ -0,0 +1,13 @@
+const firstName = "Zeeshan";
+const fvtColor = "Black";
+
+let location = "Pakistan";
+let mood = "Happy"
+
+console.log(firstName)
+console.log(fvtColor)
+console.log(location)
+console.log(mood)
+
+mood = "Sad";
+console.log(mood);
