30fd2badf5268622fbfc8fb1a3ef7abc993f1b9e
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-04-28 22:57:36 +0500

    exponents in js , bmi example

---
+++ 03_JAVASCRIPT/02_Variables/4_exponents.js
@@ -0,0 +1,17 @@
+let score = 0;
+
+score = 2 ** 2;     
+score = 2 ** 3;     
+score = 2 ** 4;    
+score = 2 ** 5;      
+
+console.log(score);  
+
+// BMI calculation
+let bmi;
+let mass = 68;  // in kg
+let height = 1.67;  // in meters
+
+bmi = mass / (height ** 2);
+
+console.log(bmi);
