f4be328945f532ebda900ec941e21fbdf47b3a61
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-04-28 23:04:18 +0500

    planet weight exercise

---
+++ 03_JAVASCRIPT/02_Variables/5_planet_weight.js
@@ -0,0 +1,7 @@
+let earth_weight = 149;
+let mars_weight;
+
+mars_weight = earth_weight * 0.38;
+
+console.log("Your weight on Earth is", earth_weight );
+console.log("Your weight on Mars is", mars_weight );
