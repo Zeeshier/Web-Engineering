0c0a6b359d01332359cea584e17f3ff4e651de87
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-04-30 22:28:38 +0500

    duck duck goose while loop

---
+++ 03_JAVASCRIPT/04_Loops/1_duck_duck_goose.js
@@ -0,0 +1,8 @@
+let random_number = Math.floor(Math.random() *10);
+
+while (random_number != 7) {
+    console.log("Duck 🦆");
+    random_number = Math.floor(Math.random() *10);
+}
+console.log("Goose! 🦢");
+
