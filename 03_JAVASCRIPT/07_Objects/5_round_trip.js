be9069336cf4beb513cb9b95395406345698c987
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-05-06 22:41:37 +0500

    round trip

---
+++ 03_JAVASCRIPT/07_Objects/5_round_trip.js
@@ -0,0 +1,55 @@
+
+const departTripTicket = {
+    name: "Brandon",
+    from: "Pittsburgh, PA",
+    to: "Augsburg, Germany",
+    businessClass: false,
+    upgrade() {
+      if (this.businessClass) {
+        console.log("Your ticket is already business class!");
+      } else {
+        this.businessClass = true;
+        console.log("Ticket upgraded to business class!");
+      }
+    },
+    leaveTime: 12,
+    arriveTime: 23,
+    flightTime() {
+      let flightTime = this.arriveTime - this.leaveTime;
+      if (flightTime < 0) {
+        flightTime += 24;
+      }
+      console.log(flightTime + " hours");
+    }
+  };
+  
+  const returnTripTicket = {
+    name: "Brandon",
+    from: "Augsburg, Germany",
+    to: "Pittsburgh, PA",
+    businessClass: true,
+    upgrade() {
+      if (this.businessClass) {
+        console.log("Your ticket is already business class!");
+      } else {
+        this.businessClass = true;
+        console.log("Ticket upgraded to business class!");
+      }
+    },
+    leaveTime: 24,
+    arriveTime: 4,
+    flightTime() {
+      let flightTime = this.arriveTime - this.leaveTime;
+      if (flightTime < 0) {
+        flightTime += 24;
+      }
+      console.log(flightTime + " hours");
+    }
+  };
+  
+  // Upgrade and check flight times
+  departTripTicket.upgrade();
+  returnTripTicket.upgrade();
+  
+  departTripTicket.flightTime();
+  returnTripTicket.flightTime();
